9761fec011e5e7ecf0a6942d307acde833ba6a5a
Christoph Bonitz <christoph.bonitz@gmail.com>
2015-06-16 23:48:30 +0200

    more tests

--- src/test/java/com/christophbonitz/logfiles/LogfilesZipServletTest.java
+++ src/test/java/com/christophbonitz/logfiles/LogfilesZipServletTest.java
@@ -15,30 +15,37 @@ import java.util.zip.ZipInputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 
-import com.christophbonitz.logfiles.LogfilesZipServlet;
-
 public class LogfilesZipServletTest {
+	private static final Logger LOGGER = Logger.getLogger(LogfilesZipServlet.class.getName());
 	
-	LogfilesZipServlet servlet = new LogfilesZipServlet();
-	ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	Logger LOGGER = Logger.getLogger(LogfilesZipServlet.class.getName());
+	private static final String CATALINA_BASE = "catalina.base";
+	private static final String PACKAGE_WITH_ONE_FILE = "onefile";
+	private static final String PACKAGE_WITH_TWO_FILES = "twofiles";
+	private static final String PACKAGE_WITHOUT_LOG_DIR = "nologs";
+	
+	private LogfilesZipServlet servlet;
+	private ByteArrayOutputStream bos;
+	
+	@Before
+	public void before() {
+		servlet = new LogfilesZipServlet();
+		bos = new ByteArrayOutputStream();
+	}
 	
 	@Test
-	public void testDoGet() throws ServletException, IOException {
-		HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
-		HttpServletResponse resp = getServletResponse();
-		setPath();
+	public void testOneFile() throws ServletException, IOException {
+		setCatalinaBase(PACKAGE_WITH_ONE_FILE);
 		
-		servlet.doGet(req, resp);
+		HttpServletResponse resp = doGetMockResponse();
 		
 		Mockito.verify(resp, Mockito.never()).sendError(Mockito.anyInt());
 		ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
@@ -50,35 +57,58 @@ public class LogfilesZipServletTest {
 			IOUtils.closeQuietly(zis);
 		}
 	}
+	
+	@Test
+	public void testTwoFiles() throws ServletException, IOException {
+		setCatalinaBase(PACKAGE_WITH_TWO_FILES);
+		
+		HttpServletResponse resp = doGetMockResponse();
+		
+		Mockito.verify(resp, Mockito.never()).sendError(Mockito.anyInt());
+		ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+		ZipInputStream zis = new ZipInputStream(bis);
+		try {
+			assertNextEntry(zis, "catalina.err", "catalina error content");
+			assertNextEntry(zis, "catalina.out", "catalina content");
+			assertNull("Not just one entry", zis.getNextEntry());
+		} finally {
+			IOUtils.closeQuietly(zis);
+		}
+	}
 
 	@Test
 	public void testContentType() throws ServletException, IOException {
-		HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
-		HttpServletResponse resp = getServletResponse();
-		setPath();
+		setCatalinaBase(PACKAGE_WITH_ONE_FILE);
 		
-		servlet.doGet(req, resp);
+		HttpServletResponse resp = doGetMockResponse();
 		
 		Mockito.verify(resp).setHeader("Content-Type", "application/octet-stream");
 	}
 	
 	@Test
-	public void testNoCatalinaBase() throws ServletException, IOException {
-		HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
-		HttpServletResponse resp = getServletResponse();
+	public void testNoCatalinaBaseVariable() throws ServletException, IOException {
+		ensureCatalinaBaseIsNotSet();
 		
-		servlet.doGet(req, resp);
+		HttpServletResponse resp = doGetMockResponse();
+		
+		Mockito.verify(resp).sendError(Matchers.eq(500), Matchers.anyString());
+	}
+	
+	@Test
+	public void testNoCatalinaBaseDirectory() throws ServletException, IOException {
+		// This directory doesn't exist
+		System.setProperty(CATALINA_BASE, "/" + UUID.randomUUID());
+		
+		HttpServletResponse resp = doGetMockResponse();
 		
 		Mockito.verify(resp).sendError(Matchers.eq(500), Matchers.anyString());
 	}
 	
 	@Test
 	public void testNoLogdir() throws ServletException, IOException {
-		HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
-		HttpServletResponse resp = getServletResponse();
-		System.setProperty("catalina.base", "/" + UUID.randomUUID());
+		setCatalinaBase(PACKAGE_WITHOUT_LOG_DIR);
 		
-		servlet.doGet(req, resp);
+		HttpServletResponse resp = doGetMockResponse();
 		
 		Mockito.verify(resp).sendError(Matchers.eq(500), Matchers.anyString());
 	}
@@ -86,24 +116,28 @@ public class LogfilesZipServletTest {
 	@After
 	public void after() {
 		IOUtils.closeQuietly(bos);
-		// if set during test, we need to unset it.
-		System.getProperties().remove("catalina.base");
 	}
 
-	private void setPath() {
-		URL catalinaTestUrl = LogfilesZipServletTest.class.getResource(".");
-		System.setProperty("catalina.base", catalinaTestUrl.getPath());
+	private void setCatalinaBase(String subpackage) {
+		URL catalinaTestUrl = LogfilesZipServletTest.class.getResource(String.format("./%s/", subpackage));
+		System.setProperty(CATALINA_BASE, catalinaTestUrl.getPath());
 		LOGGER.info("catalina base set to " + catalinaTestUrl.getPath());
 	}
 
-	private void assertNextEntry(ZipInputStream zis, String filename, String content) throws IOException {
-		ZipEntry entry = zis.getNextEntry();
-		assertEquals("Didn't find " + filename, filename, entry.getName());
-		ByteArrayOutputStream eos = new ByteArrayOutputStream();
-		IOUtils.copy(zis, eos);
-		IOUtils.closeQuietly(eos);
-		String receivedContent = new String(eos.toByteArray(), Charset.availableCharsets().get("UTF-8"));
-		assertEquals(filename + " didn't contain the expected content.", content, receivedContent);
+	private void ensureCatalinaBaseIsNotSet() {
+		if (System.getProperty(CATALINA_BASE) != null) {
+			System.getProperties().remove(CATALINA_BASE);
+			LOGGER.info("unsetting " + CATALINA_BASE);
+		}
+	}
+
+	private HttpServletResponse doGetMockResponse() throws IOException,
+			ServletException {
+		HttpServletResponse resp = getServletResponse();
+		
+		servlet.doGet(null, resp);
+		
+		return resp;
 	}
 
 	private HttpServletResponse getServletResponse() throws IOException {
@@ -116,5 +150,15 @@ public class LogfilesZipServletTest {
 		};
 		Mockito.when(resp.getOutputStream()).thenReturn(os);
 		return resp;
+	}
+
+	private void assertNextEntry(ZipInputStream zis, String filename, String content) throws IOException {
+		ZipEntry entry = zis.getNextEntry();
+		assertEquals("Didn't find " + filename, filename, entry.getName());
+		ByteArrayOutputStream eos = new ByteArrayOutputStream();
+		IOUtils.copy(zis, eos);
+		IOUtils.closeQuietly(eos);
+		String receivedContent = new String(eos.toByteArray(), Charset.availableCharsets().get("UTF-8"));
+		assertEquals(filename + " didn't contain the expected content.", content, receivedContent);
 	}
 }
